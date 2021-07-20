ffcd59da5d0baadada62eef05f06dd31e0164255
Wellington <wellingtoncarvalho11@gmail.com>
2021-07-19 23:55:48 -0300

    Inicializando Login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
+import { useHistory, Link } from 'react-router-dom';
+import Aluno from '../../assets/login/aluno.png';
+import LogoFatec from '../../assets/logoFatec.png';
+import './style.scss'
 
+const Login = () => {
 
-const Login =  () => {
+    const history = useHistory();
+
 
     return (
-        <div>
+        <div className='flex flex-direction'>
+            <form className='form'>
+                <img src={Aluno} alt='Aluno' />
 
+                <div className='box-text'>
+                    <ul>
+                        <Link to='cadastro' className='cor-primeiro-acesso'>Primeiro Acesso</Link>
+                    </ul>
+                    <ul>
+                        <Link to='/esqueci-minha-senha' className='cor-esqueci-senha'>Esqueci a minha senha</Link>
+                    </ul>
+                </div>
+
+            </form>
+            <div className='box-logo'>
+                <img src={LogoFatec} alt='Logo Fatec' />
+            </div>
         </div>
-    )
-    
+    );
+
 
 }
 
