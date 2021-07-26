f750a35535bb24aeef687b2e869c30f0df4fedcb
nascito_nicholas <nascito.nicholas@gmail.com>
2021-07-26 13:03:13 -0300

    feat: calendario

---
+++ src/pages/calendario/Calendario.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { FiDownload } from 'react-icons/fi';
+import './Calendario.scss';
+
+const Calendario = () => {
+
+  return (
+    <div className='flex flex-column'>
+        <a href='https://fatec-app.s3.sa-east-1.amazonaws.com/CalendarioEscolar+(1).pdf' target='_blank' rel="noreferrer" download >
+          <button type="button">
+            <FiDownload size={45} color="#264653" />
+            <p>Calendario.pdf</p>
+          </button>
+        </a>
+    </div>
+  );
+};
+
+export default Calendario;
