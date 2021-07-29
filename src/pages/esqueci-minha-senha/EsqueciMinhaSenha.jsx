8804543d32d316747779a474966cdecffcb4db26
Wellington <wellingtoncarvalho11@gmail.com>
2021-07-29 01:14:45 -0300

    feat: tela de recuperacao de senha

--- src/pages/esqueci-minha-senha/EsqueciMinhaSenha.jsx
+++ src/pages/esqueci-minha-senha/EsqueciMinhaSenha.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import VoltarSair from '../../shared/components/VoltarSair';
 import { Button } from '@material-ui/core';
 import './EsqueciMinhaSenha.scss'
+import TextField from "@material-ui/core/TextField";
+import InputLabel from '@material-ui/core/InputLabel';
 
 
 const EsqueciMinhaSenha = () => {
@@ -11,6 +13,9 @@ const EsqueciMinhaSenha = () => {
         nrMatricula: "",
     });
 
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+    }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -26,8 +31,29 @@ const EsqueciMinhaSenha = () => {
         <form>
             <VoltarSair flagVoltar={true} />
             <div className='flex flex-column flex-center' >
-                <p>E-mail</p>
-                <p>Matricula</p>
+                <div className='flex input-email'>
+                    <InputLabel htmlFor="input-email" className='label-email'>
+                        E-mail
+                    </InputLabel>
+                    <TextField
+                        id="input-email"
+                        onChange={handleChange("email")}
+                        variant="outlined"
+                        className='box-email'
+                    />
+                </div>
+                <div className='flex input-matricula'>
+                    <InputLabel htmlFor="input-matricula" className='label-matricula'>
+                        Matrícula
+                    </InputLabel>
+                    <TextField
+                        id="input-matricula"
+                        onChange={handleChange("nrMatricula")}
+                        variant="outlined"
+                        className='box-matricula'
+                    />
+                </div>
+
             </div>
             <div className='container-button'>
                 <Button variant="contained" className='button-recuperar-senha' onClick={handleSubmit}>
