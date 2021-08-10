218402168bf912cd1f0b6c80d1325e9075ac2597
nascito_nicholas <nascito.nicholas@gmail.com>
2021-08-10 13:31:41 -0300

    feat: estrutura curricular

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MenuPrincipal from '../pages/menu/MenuPrincipal';
 import Login from '../pages/login/Login';
 import ReenvioProtocolo from '../pages/reenvio-protocolo/ReenvioProtocolo';
+import EstruturaCurricular from '../pages/estrutura-curricular/EstruturaCurricular';
 import Calendario from '../pages/calendario/Calendario';
 import AvaliacoesFinais from '../pages/avaliacoes-finais/AvaliacoesFinais';
 import EsqueciMinhaSenha from '../pages/esqueci-minha-senha/EsqueciMinhaSenha';
@@ -25,9 +26,9 @@ const Routes = () => {
         <Route path='/protocolo-rematricula' component={MenuPrincipal}/>
       <Route path='/rematricula' component={MenuPrincipal}/>*/}
         <Route path='/reenvio-protocolo' component={ReenvioProtocolo}/>
-       {/*} <Route path='/matriculas-confirmadas' component={MenuPrincipal}/>
-        <Route path='/estrutura-curricular' component={MenuPrincipal}/>
-    <Route path='/historico-disciplinar' component={MenuPrincipal}/>*/}
+       {/*} <Route path='/matriculas-confirmadas' component={MenuPrincipal}/>*/}
+        <Route path='/estrutura-curricular' component={EstruturaCurricular}/>
+    {/*<Route path='/historico-disciplinar' component={MenuPrincipal}/>*/}
         <Route path='/calendario-escolar' component={Calendario}/>
     {/*<Route path='/grades-horarias' component={MenuPrincipal}/>*/}
         <Route path='/avaliacoes-finais' component={AvaliacoesFinais}/>
