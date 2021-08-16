5c9d68d6f2a6f31b4ad93ee3bacd802d34a379f5
nascito_nicholas <nascito.nicholas@gmail.com>
2021-08-16 20:26:45 -0300

    feat: matriculas confirmadas.

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MenuPrincipal from '../pages/menu/MenuPrincipal';
 import Login from '../pages/login/Login';
 import ReenvioProtocolo from '../pages/reenvio-protocolo/ReenvioProtocolo';
+import MatriculasConfirmadas from '../pages/matriculas-confirmadas/MatriculasConfirmadas';
 import EstruturaCurricular from '../pages/estrutura-curricular/EstruturaCurricular';
 import Calendario from '../pages/calendario/Calendario';
 import GradeHoraria from '../pages/grade-horaria/GradeHoraria';
@@ -19,19 +20,15 @@ const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-        {/* <Route path='/' exact component={MenuPrincipal}/>
-        <Route path='/cadastro' component={MenuPrincipal}/>*/
-        <Route path='/esqueci-minha-senha' component={EsqueciMinhaSenha}/>}
+        {/*<Route path='/cadastro' component={MenuPrincipal}/>*/}
+        <Route path='/esqueci-minha-senha' component={EsqueciMinhaSenha}/>
         <Route path='/' exact component={Login}/>
-        {/* 
-        <Route path='/cadastro' component={MenuPrincipal}/>
-        <Route path='/esqueci-minha-senha' component={MenuPrincipal}/> */}
         <Route path='/menu-principal' component={MenuPrincipal} />
     {/* <Route path='/meus-emails' component={MenuPrincipal}/>
         <Route path='/protocolo-rematricula' component={MenuPrincipal}/>
         <Route path='/rematricula' component={MenuPrincipal}/>*/}
         <Route path='/reenvio-protocolo' component={ReenvioProtocolo}/>
-       {/*} <Route path='/matriculas-confirmadas' component={MenuPrincipal}/>*/}
+        <Route path='/matriculas-confirmadas' component={MatriculasConfirmadas}/>
         <Route path='/estrutura-curricular' component={EstruturaCurricular}/>
     {/*<Route path='/historico-disciplinar' component={MenuPrincipal}/>*/}
         <Route path='/calendario-escolar' component={Calendario}/>
