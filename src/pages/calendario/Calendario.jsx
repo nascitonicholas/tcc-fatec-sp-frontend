64340a73aae98027ca7b31c84f96ab7cf89c8924
nascito_nicholas <nascito.nicholas@gmail.com>
2021-11-03 19:05:38 -0300

    colocando voltar na pagina de calendario

--- src/pages/calendario/Calendario.jsx
+++ src/pages/calendario/Calendario.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiDownload } from 'react-icons/fi';
+import VoltarSair from '../../shared/components/VoltarSair';
 import './Calendario.scss';
 
 const Calendario = () => {
@@ -8,13 +9,16 @@ const Calendario = () => {
   localStorage.setItem('tituloHeader','Calendário Escolar')
 
   return (
-    <div className='flex flex-column'>
-        <a href='https://fatec-app.s3.sa-east-1.amazonaws.com/CalendarioEscolar+(1).pdf' target='_blank' rel="noreferrer" download >
-          <button type="button">
-            <FiDownload size={45} color="#264653" />
-            <p>Calendario.pdf</p>
-          </button>
-        </a>
+    <div>
+      <VoltarSair flagVoltar={true} /> 
+      <div className='flex flex-column'>
+          <a href='https://fatec-app.s3.sa-east-1.amazonaws.com/CalendarioEscolar+(1).pdf' target='_blank' rel="noreferrer" download >
+            <button type="button">
+              <FiDownload size={45} color="#264653" />
+              <p>Calendario.pdf</p>
+            </button>
+          </a>
+      </div>
     </div>
   );
 };
